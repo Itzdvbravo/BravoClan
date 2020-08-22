c58abdc3792532c68122792386e349e3511bbe35
Itzdvbravo <47894997+Itzdvbravo@users.noreply.github.com>
2020-08-22 22:07:31 +0530

    Error Fixing

--- src/Itzdvbravo/BravoClan/Commands/SubCommands/Kick.php
+++ src/Itzdvbravo/BravoClan/Commands/SubCommands/Kick.php
@@ -1,56 +1,93 @@
 <?php
 
-
-namespace Itzdvbravo\BravoClan\Commands\SubCommands;
-
+namespace Itzdvbravo\BravoClan;
 
 use Itzdvbravo\BravoClan\Main;
-use pocketmine\command\CommandSender;
+use Itzdvbravo\BravoClan\Database;
+use pocketmine\event\entity\EntityDamageByEntityEvent;
+use pocketmine\event\entity\EntityDamageEvent;
+use pocketmine\event\Listener;
+use pocketmine\event\player\PlayerChatEvent;
+use pocketmine\event\player\PlayerDeathEvent;
+use pocketmine\event\player\PlayerJoinEvent;
+use pocketmine\event\player\PlayerQuitEvent;
+use pocketmine\Player;
+use pocketmine\Server;
+use pocketmine\utils\Config;
 
-class Kick implements Sub{
-    private $plugin;
+class EventListener implements Listener{
 
-    public function __construct(Main $plugin){
-        $this->plugin = $plugin;
+    public function onChat(PlayerChatEvent $event){
+        #Adding all the checks just incase the player gets kicked/leaves the clan.
+        $player = $event->getPlayer();
+        if (!array_key_exists(strtolower($player->getName()), Main::$clan->chat)) return;
+        $msg = $event->getMessage();
+        $event->setCancelled(true);
+        $clan = Main::$file->getClan(Main::$clan->chat[strtolower($player->getName())]);
+        $minfo = Main::$file->getMember(strtolower($player->getName()));
+        if (!Main::$file->isInClan(strtolower($player->getName())) or $clan['clan'] !== $minfo['clan']){
+            unset(Main::$clan->chat[strtolower($player->getName())]);
+            return;
+        }
+        $members = Main::$file->clanMembers($clan['clan']);
+        foreach ($members as $member) {
+            if (Main::getInstance()->isOnline($member)) {
+                $getm = Server::getInstance()->getPlayer($member);
+                $rank = $clan['leader'] === strtolower($player->getName()) ? "leader" : "member";
+                $getm->sendMessage("§o§e[{$clan['clan']}] §a[$rank] §5{$player->getName()} §a-> §e{$msg}");
+            }
+        }
     }
 
-    public function getName(): string
-    {
-        return "kick";
-    }
-
-    /**
-     * @inheritDoc
-     */
-    public function executeSub(CommandSender $player, array $args, string $name){
-        if (!Main::$file->isInClan(strtolower($player->getName()))) {
-            $player->sendMessage(Main::getInstance()->addColour(Main::getInstance()->messages->get("not-in-clan")));
-        }  elseif (Main::$file->getClan(Main::$clan->player[strtolower($player->getName())])["leader"] !== strtolower($player->getName())){
-            $player->sendMessage(Main::getInstance()->addColour(Main::getInstance()->messages->get("not-leader")));
-        } else {
-            if (empty($args[1])){
-                $player->sendMessage("§eProvide a player to kick");
-            } elseif(strtolower($args[1]) === strtolower($player->getName())) {
-                $player->sendMessage("§eYou can't kick yourself");
-            }else {
-                if (Main::$file->isInClan(strtolower($args[1]))){
-                    if (Main::$file->getMember(strtolower($args[1]))['clan'] !== Main::$file->getClan
-                        (Main::$clan->player[strtolower($player->getName())])){
-                        $player->sendMessage("§eThat player isn't in your clan");
-                    } else {
-                        $clan = Main::$file->getClan(Main::$file->getMember(strtolower($args[1]))["clan"]);
-                        $member = Main::$file->getMember(strtolower($args[1]));
-                        Main::$file->setKills($clan["clan"], $clan["kills"] - $member["kills"]);
-                        Main::$file->setDeaths($clan["clan"], $clan["deaths"] - $member["deaths"]);
-                        Main::$file->removeMember(strtolower($args[1]));
-                        Main::$file->setTm($clan['clan'], $clan['tm'] - 1);
-                        if ($this->plugin->isOnline($args[1])){
-                            unset(Main::$clan->player[strtolower($args[1])]);
-                        }
-                        $player->sendMessage("§eThat player have been removed");
+    public function onDamage(EntityDamageEvent $event){
+        if ($event instanceof EntityDamageByEntityEvent){
+            $player = $event->getEntity();
+            $hitter = $event->getDamager();
+            if ($player instanceof Player && $hitter instanceof Player){
+                if (!Main::getInstance()->inPvpWorld($player)){
+                    return;
+                }
+                if (Main::$file->isInClan(strtolower($player->getname())) && Main::$file->isInClan(strtolower($hitter->getname()))) {
+                    if (Main::$clan->player[strtolower($player->getName())] === Main::$clan->player[strtolower($hitter->getName())]) {
+                        $event->setCancelled(true);
                     }
-                } else {
-                    $player->sendMessage("§eThat player isn't in any gang");
+                }
+            }
+        }
+    }
+    public function onJoin(PlayerJoinEvent $event){
+        $player = $event->getPlayer();
+        if (Main::$file->isInClan(strtolower($player->getname()))){
+            $clan = Main::$file->getClan(Main::$file->getMember(strtolower($player->getName()))['clan']);
+            Main::$clan->player[strtolower($player->getName())] = $clan["clan"];
+        }
+    }
+
+    public function onLeave(PlayerQuitEvent $event){
+        $player = $event->getPlayer();
+        if (Main::$file->isInClan(strtolower($player->getname()))) {
+            unset(Main::$clan->player[strtolower($player->getName())]);
+        }
+    }
+
+    public function onKill(PlayerDeathEvent $event){
+        $player = $event->getPlayer();
+        if ($player instanceof Player) {
+            if (!Main::getInstance()->inPvpWorld($player)){
+                return;
+            }
+            $cause = $player->getLastDamageCause();
+            if ($cause instanceof EntityDamageByEntityEvent) {
+                $killer = $cause->getDamager();
+                if ($killer instanceof Player) {
+                    if (Main::$file->isInClan(strtolower($killer->getname()))) {
+                        $clan = Main::$file->getClan(Main::$clan->player[strtolower($killer->getName())]);
+                        Main::$clan->onClanMemberKill($clan, $killer);
+                    }
+                    if (Main::$file->isInClan(strtolower($player->getname()))) {
+                        $clan = Main::$file->getClan(Main::$clan->player[strtolower($player->getName())]);
+                        Main::$clan->onClanMemberDeath($clan, $player);
+                    }
                 }
             }
         }
